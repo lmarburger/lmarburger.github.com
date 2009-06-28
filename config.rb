360b15f78cf6e81a58398a24f775f70c32185d43
Larry Marburger <larry@marburger.cc>
2009-06-28 15:38:26 -0400

    Moved the Sass directory to one Jekyll will ignore.

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 project_type = :stand_alone
 css_dir = "stylesheets"
-sass_dir = "stylesheets/sass"
+sass_dir = "_sass"
 images_dir = "images"
 output_style = :compact
 # To enable relative image paths using the images_url() function:
